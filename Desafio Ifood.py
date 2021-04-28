fc8ccd77c06286f86f5a36f31e302a5363935627
Victor Lincoln <72924572+victorlincoln@users.noreply.github.com>
2021-04-28 15:39:39 -0300

    Add files via upload

---
+++ Desafio Ifood.py
@@ -0,0 +1,25 @@
+print("Ranking dos restaurantes: Próximos ao endereço de entrega, realizado por critérios de avaliação e valor do frete:" )
+print()
+
+kibon_Sorveteria_Saude = [4.9, 6.99, "Kibon Sorveteria - Saúde" ]
+Makis_Place_Saude = [4.7, 7.99, "Makis Place - Saúde"]
+Sukiya_Saude = [4.6, 7.99, "Sukiya - Saúde"]
+Giraffas_Carrefour_Metrocar = [4.4, 5.99, "Giraffas Carrefour Metrocar"]
+A_feijoada = [4.4, 9.90, "A feijoada"]
+Viena_Shopping_Santa_Cruz = [4.4, 12.49, "Viena Shopping Santa Cruz" ]
+
+lista_restaurantes = [kibon_Sorveteria_Saude, Makis_Place_Saude, Sukiya_Saude, Giraffas_Carrefour_Metrocar, A_feijoada, Viena_Shopping_Santa_Cruz]
+
+for x in lista_restaurantes:
+    if x[0] >=4.9:
+        print("1º: {} (nota {} com frete de R$ {})".format(x[2], x[0], x[1]))
+    elif x[0] >= 4.7:
+        print("2º: {} (nota {} com frete de R$ {})".format(x[2], x[0], x[1]))
+    elif x[0] >= 4.6 and x[1] <= 7.99:
+        print("3º: {} (nota {} com frete de R$ {})".format(x[2], x[0], x[1]))
+    elif x[0] >= 4.4 and x[1] <=5.99:
+        print("4º: {} (nota {} com frete de R$ {})".format(x[2], x[0], x[1]))
+    elif x[0] >= 4.4 and x[1] <= 9.90:
+        print("5º: {} (nota {} com frete de R$ {})".format(x[2], x[0], x[1]))
+    elif x[0] >= 4.4 and x[1] <=12.49:
+        print("6º: {} (nota {} com frete de R$ {})".format(x[2], x[0], x[1]))
